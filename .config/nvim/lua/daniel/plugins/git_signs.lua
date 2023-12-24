56ec596503f4d25b99226e383578c132bdf61348
Daniel Ohayon <danielohayon444@gmail.com>
2023-12-24 12:56:30 +0200

    added keyboard shortcuts

--- .config/nvim/lua/daniel/plugins/git_signs.lua
+++ .config/nvim/lua/daniel/plugins/git_signs.lua
@@ -29,19 +29,38 @@ return {
 				end, {expr=true})
 
 				-- Actions
-				map('n', '<leader>hs', gs.stage_hunk, { desc = "Stage Hunk" })
-				map('n', '<leader>hr', gs.reset_hunk, { desc = "Reset Hunk" })
-				map('v', '<leader>hs', function() gs.stage_hunk {vim.fn.line('.'), vim.fn.line('v')} end, { desc = "Stage Hunk" })
-				map('v', '<leader>hr', function() gs.reset_hunk {vim.fn.line('.'), vim.fn.line('v')} end, { desc = "Reset Hunk" })
-				map('n', '<leader>hS', gs.stage_buffer, { desc = "Stage Buffer" })
-				map('n', '<leader>hu', gs.undo_stage_hunk, {desc = "Undo Stage Hunk" })
-				map('n', '<leader>hR', gs.reset_buffer, {desc = "Reset Buffer" })
-				map('n', '<leader>hp', gs.preview_hunk, {desc = "Preview Hunk" })
-				map('n', '<leader>hb', function() gs.blame_line{full=true} end, { desc = "Blame Line" })
-				map('n', '<leader>htb', gs.toggle_current_line_blame, {desc = "Toggle Current Line Blame" })
-				map('n', '<leader>hd', gs.diffthis, {desc = "Diff File" })
-				map('n', '<leader>hD', function() gs.diffthis('~') end, {desc = "Diff File ~" })
-				map('n', '<leader>htd', gs.toggle_deleted, { desc = "Toggle Deleted" })
+				map('n', '<leader>ghs', gs.stage_hunk, { desc = "Stage Hunk" })
+				map('n', '<leader>ghr', gs.reset_hunk, { desc = "Reset Hunk" })
+				map('v', '<leader>ghs', function() gs.stage_hunk {vim.fn.line('.'), vim.fn.line('v')} end, { desc = "Stage Hunk" })
+				map('v', '<leader>ghr', function() gs.reset_hunk {vim.fn.line('.'), vim.fn.line('v')} end, { desc = "Reset Hunk" })
+				map('n', '<leader>ga', gs.stage_buffer, { desc = "Stage Buffer" })
+				map('n', '<leader>ghu', gs.undo_stage_hunk, {desc = "Undo Stage Hunk" })
+				map('n', '<leader>gR', gs.reset_buffer, {desc = "Reset Buffer" })
+				map('n', '<leader>ghp', gs.preview_hunk, {desc = "Preview Hunk" })
+				map('n', '<leader>gb', function() gs.blame_line{full=true} end, { desc = "Blame Line" })
+				map('n', '<leader>gtb', gs.toggle_current_line_blame, {desc = "Toggle Current Line Blame" })
+				map('n', '<leader>gd', gs.diffthis, {desc = "Diff File" })
+				map('n', '<leader>gD', function() gs.diffthis('~') end, {desc = "Diff File ~" })
+				map('n', '<leader>gtd', gs.toggle_deleted, { desc = "Toggle Deleted" })
+
+				-- Telescope Functions 
+
+				map('n', '<leader>gc', "<cmd>Telescope git_bcommits<cr>", { desc = "Toggle Deleted" })
+				map('n', '<leader>gs', "<cmd>Telescope git_status<cr>", { desc = "Toggle Deleted" })
+
+
+				-- Add Lazygit keymapping 
+				local Terminal  = require('toggleterm.terminal').Terminal
+				local lazygit = Terminal:new({ cmd = "lazygit", hidden = true , direction="float", 
+					on_open = function(_)
+						vim.cmd "startinsert!"
+					end,})
+
+				function _lazygit_toggle()
+					lazygit:toggle()
+				end
+
+				vim.api.nvim_set_keymap("n", "<leader>gl", "<cmd>lua _lazygit_toggle()<CR>", {noremap = true, silent = true})
 
 				-- Text object
 				map({'o', 'x'}, 'ih', ':<C-U>Gitsigns select_hunk<CR>')
--- .config/nvim/lua/daniel/plugins/telescope.lua
+++ .config/nvim/lua/daniel/plugins/telescope.lua
@@ -29,8 +29,16 @@ return {
     local keymap = vim.keymap -- for conciseness
 
     keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
-    keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
+    keymap.set("n", "<leader>fo", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
     keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
-    keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
+    keymap.set("n", "<leader>fu", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
+    keymap.set("n", "<leader>fh", "<cmd>Telescope current_buffer_fuzzy_find<cr>", { desc = "Search In Current Buffer" })
+    keymap.set("n", "<leader>fr", "<cmd>Telescope resume<cr>", { desc = "Continue Previous Search" })
+    keymap.set("n", "<leader>fp", "<cmd>Telescope pickers<cr>", { desc = "Previous Searches" })
+
+    keymap.set("n", "<leader>fv", "<cmd>Telescope command_history<cr>", { desc = "Command History" })
+    keymap.set("n", "<leader>fj", "<cmd>Telescope jumplist<cr>", { desc = "*Jump History*" })
+    keymap.set("n", "<leader>fk", "<cmd>Telescope keymaps<cr>", { desc = "Keymaps" })
+
   end,
 }
