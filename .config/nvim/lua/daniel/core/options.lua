e043a849a375d7c48f23fbbe407bc900e4626108
Daniel Ohayon <dohayon@habana.ai>
2023-12-30 14:16:25 +0200

    updates scrolloff

--- .config/nvim/lua/daniel/core/options.lua
+++ .config/nvim/lua/daniel/core/options.lua
@@ -33,7 +33,7 @@ opt.splitright = true -- split vertical window to the right
 opt.splitbelow = true -- split horizontal window to the bottom
 
 
-vim.opt.scrolloff = 10
+vim.opt.scrolloff = 15
 vim.cmd.colorscheme "catppuccin-macchiato" -- catppuccin-latte, catppuccin-frappe, catppuccin-macchiato, catppuccin-mocha
 
 vim.opt.fillchars = {eob = " "}
