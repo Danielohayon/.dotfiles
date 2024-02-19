753e34387d5f328c38371b856f1c9797d0968cdb
Daniel Ohayon <dohayon@habana.ai>
2024-02-19 16:21:19 +0200

    packages

---
+++ apt_install_packages.sh
@@ -0,0 +1,11 @@
+# Crutial
+sudo apt -y install git
+
+
+# Usful
+sudo apt -y install exa # A better ls
+
+
+# Nice To have
+sudo apt -y install xsel # Fix yanking problem in Tmux
+npm install -g tldr
