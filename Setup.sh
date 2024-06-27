a9f2c8c80b32a05d79f614383b8217ea58d67f89
Daniel Ohayon <danielohayon444@gmail.com>
2024-06-27 21:31:40 +0300

    Update Setup.sh

--- Setup.sh
+++ Setup.sh
@@ -3,3 +3,6 @@ sudo rm -rf /opt/nvim
 sudo tar -C /opt -xzf nvim-linux64.tar.gz
 
 echo 'export PATH="$PATH:/opt/nvim-linux64/bin"' >> ~/.bashrc
+
+rm -rf ~/.config/nvim
+ln -s ~/.dotfiles/.config/nvim ~/.config/nvim
