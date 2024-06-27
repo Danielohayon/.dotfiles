78278346d497cb064d061889826076817bff1650
Daniel Ohayon <danielohayon444@gmail.com>
2024-06-27 21:33:09 +0300

    Update Setup.sh

--- Setup.sh
+++ Setup.sh
@@ -1,8 +1,13 @@
+
+sudo apt install fzf
+
+# Install nvim
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
 sudo rm -rf /opt/nvim
 sudo tar -C /opt -xzf nvim-linux64.tar.gz
 
 echo 'export PATH="$PATH:/opt/nvim-linux64/bin"' >> ~/.bashrc
 
+# link nvim and tmux configurations
 rm -rf ~/.config/nvim
 ln -s ~/.dotfiles/.config/nvim ~/.config/nvim
