33d39bdbbad31c424c56986aa3ba0009327b85b9
Daniel Ohayon <danielohayon444@gmail.com>
2024-07-01 11:51:24 +0300

    added keymaps

--- .config/nvim/lua/daniel/core/keymaps.lua
+++ .config/nvim/lua/daniel/core/keymaps.lua
@@ -18,6 +18,7 @@ keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" }) --
 keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" }) -- split window horizontally
 keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" }) -- make split windows equal width & height
 keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" }) -- close current split window
+keymap.set("n", "<leader>li2", "<cmd>set autoindent expandtab tabstop=2 shiftwidth=2<CR>", { desc = "Change indentation level to 2" }) -- close current split window
 
 -- keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" }) -- open new tab
 -- keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" }) -- close current tab
