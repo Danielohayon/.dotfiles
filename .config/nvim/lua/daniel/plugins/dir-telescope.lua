e5c429088056101e21dc64438d7c28cd90093c1d
Daniel Ohayon <danielohayon444@gmail.com>
2024-07-31 10:44:20 +0300

    added plugin to search in specific folders

---
+++ .config/nvim/lua/daniel/plugins/dir-telescope.lua
@@ -0,0 +1,13 @@
+return {
+  "princejoogie/dir-telescope.nvim",
+  -- telescope.nvim is a required dependency
+  requires = {"nvim-telescope/telescope.nvim"},
+  config = function()
+    require("dir-telescope").setup({
+      -- these are the default options set
+      hidden = true,
+      no_ignore = false,
+      show_preview = true,
+    })
+  end,
+}
