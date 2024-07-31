c07d4236ea2c8390f12d307cbac19f510887fda5
Daniel Ohayon <danielohayon444@gmail.com>
2024-07-31 10:46:18 +0300

    added oil

---
+++ .config/nvim/lua/daniel/plugins/oil.lua
@@ -0,0 +1,7 @@
+return {
+  'stevearc/oil.nvim', -- plugin that let's you edit your files like a normal vim buffer
+  opts = {},
+  -- Optional dependencies
+  dependencies = { { "echasnovski/mini.icons", opts = {} } },
+  -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
+}
