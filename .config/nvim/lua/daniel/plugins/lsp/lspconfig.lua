6a4fc87070e38c1368a00209b97a1f79be7e4c39
Daniel Ohayon <danielohayon@Daniels-MacBook-Pro.local>
2025-12-01 18:33:46 +0200

    updated lsp config

--- .config/nvim/lua/daniel/plugins/lsp/lspconfig.lua
+++ .config/nvim/lua/daniel/plugins/lsp/lspconfig.lua
@@ -4,186 +4,78 @@ return {
   dependencies = {
     "hrsh7th/cmp-nvim-lsp",
     { "antosha417/nvim-lsp-file-operations", config = true },
-		"nvim-lua/plenary.nvim"
+    "nvim-lua/plenary.nvim"
   },
   config = function()
-    -- Suppress lspconfig deprecation warning (still works fine, migration can be done later)
-    local orig_notify = vim.notify
-    vim.notify = function(msg, ...)
-      if msg and msg:match("lspconfig.*deprecated") then return end
-      return orig_notify(msg, ...)
-    end
-
-    local lspconfig = require("lspconfig")
-
-    -- Restore original notify
-    vim.notify = orig_notify
-
-    -- import cmp-nvim-lsp plugin
     local cmp_nvim_lsp = require("cmp_nvim_lsp")
-
-    local keymap = vim.keymap -- for conciseness
-
-    local opts = { noremap = true, silent = true }
-    local on_attach = function(client, bufnr)
-      opts.buffer = bufnr
-
-      -- set keybinds
-      opts.desc = "Show LSP references"
-      keymap.set("n", "gr", "<cmd>Telescope lsp_references<CR>", opts) -- show definition, references
-
-      -- opts.desc = "Go to declaration"
-      -- keymap.set("n", "gD", vim.lsp.buf.declaration, opts) -- go to declaration
-
-      opts.desc = "Show LSP definitions"
-      keymap.set("n", "gd", "<cmd>Telescope lsp_definitions<CR>", opts) -- show lsp definitions
-
-      opts.desc = "Show LSP type definitions"
-      keymap.set("n", "gt", "<cmd>Telescope lsp_type_definitions<CR>", opts) -- show lsp type definitions
-
-      opts.desc = "Incoming Calls"
-      keymap.set("n", "gi", "<cmd>Telescope lsp_incoming_calls<CR>", opts) -- show lsp implementations
-
-      opts.desc = "Outgoing Calls"
-      keymap.set("n", "go", "<cmd>Telescope lsp_outgoing_calls<CR>", opts) -- show lsp implementations
-
-			opts.desc = "Add workspace Folder"
-			vim.keymap.set('n', '<space>lwa', vim.lsp.buf.add_workspace_folder, opts)
-			opts.desc = "Remove Workspace Folder"
-			vim.keymap.set('n', '<space>lwr', vim.lsp.buf.remove_workspace_folder, opts)
-			opts.desc = "List Workspace Folders"
-			vim.keymap.set('n', '<space>lwl', function()
-				print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-			end, opts)
-
-      opts.desc = "See available code actions"
-      keymap.set({ "n", "v" }, "<leader>la", vim.lsp.buf.code_action, opts) -- see available code actions, in visual mode will apply to selection
-
-      opts.desc = "Smart rename"
-      keymap.set("n", "<leader>lr", vim.lsp.buf.rename, opts) -- smart rename
-
-      opts.desc = "Show buffer diagnostics"
-      -- keymap.set("n", "<leader>D", "<cmd>Telescope diagnostics bufnr=0<CR>", opts) -- show  diagnostics for file
-      keymap.set("n", "<leader>lD", "<cmd>Telescope diagnostics bufnr=0<CR>", opts) -- show  diagnostics for file
-
-      opts.desc = "Show line diagnostics"
-      -- keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts) -- show diagnostics for line
-      keymap.set("n", "<leader>ld", vim.diagnostic.open_float, opts) -- show diagnostics for line
-
-      opts.desc = "Go to previous diagnostic"
-      keymap.set("n", "<leader>lp", vim.diagnostic.goto_prev, opts) -- jump to previous diagnostic in buffer
-
-      opts.desc = "Go to next diagnostic"
-      keymap.set("n", "<leader>ln", vim.diagnostic.goto_next, opts) -- jump to next diagnostic in buffer
-
-      opts.desc = "Show documentation for what is under cursor"
-      keymap.set("n", "K", vim.lsp.buf.hover, opts) -- show documentation for what is under cursor
-      -- keymap.set("n", "<leader>li", vim.lsp.buf.hover, opts) -- show documentation for what is under cursor
-
-      opts.desc = "Restart LSP"
-      keymap.set("n", "<leader>lx", ":LspRestart<CR>", opts) -- mapping to restart lsp if necessary
-
-			keymap.set("n", "<leader>ls", "<cmd>Telescope spell_suggest<cr>", { desc = "Spell Suggest" })
-    end
-
-    -- used to enable autocompletion (assign to every lsp server config)
     local capabilities = cmp_nvim_lsp.default_capabilities()
 
     -- Change the Diagnostic symbols in the sign column (gutter)
-    -- (not in youtube nvim video)
-    local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
+    local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
     for type, icon in pairs(signs) do
       local hl = "DiagnosticSign" .. type
       vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
     end
 
-    -- configure typescript server with plugin
-    -- lspconfig["tsserver"].setup({
-    --   capabilities = capabilities,
-    --   on_attach = on_attach,
-    -- })
+    -- LSP keymaps (set up on LspAttach)
+    vim.api.nvim_create_autocmd("LspAttach", {
+      group = vim.api.nvim_create_augroup("UserLspConfig", { clear = true }),
+      callback = function(ev)
+        local opts = { buffer = ev.buf, noremap = true, silent = true }
 
-    -- configure tailwindcss server
-    -- lspconfig["tailwindcss"].setup({
-    --   capabilities = capabilities,
-    --   on_attach = on_attach,
-    -- })
+        vim.keymap.set("n", "gr", "<cmd>Telescope lsp_references<CR>", vim.tbl_extend("force", opts, { desc = "Show LSP references" }))
+        vim.keymap.set("n", "gd", "<cmd>Telescope lsp_definitions<CR>", vim.tbl_extend("force", opts, { desc = "Show LSP definitions" }))
+        vim.keymap.set("n", "gt", "<cmd>Telescope lsp_type_definitions<CR>", vim.tbl_extend("force", opts, { desc = "Show LSP type definitions" }))
+        vim.keymap.set("n", "gi", "<cmd>Telescope lsp_incoming_calls<CR>", vim.tbl_extend("force", opts, { desc = "Incoming Calls" }))
+        vim.keymap.set("n", "go", "<cmd>Telescope lsp_outgoing_calls<CR>", vim.tbl_extend("force", opts, { desc = "Outgoing Calls" }))
 
-    -- configure svelte server
-    -- lspconfig["svelte"].setup({
-    --   capabilities = capabilities,
-    --   on_attach = function(client, bufnr)
-    --     on_attach(client, bufnr)
-    --
-    --     vim.api.nvim_create_autocmd("BufWritePost", {
-    --       pattern = { "*.js", "*.ts" },
-    --       callback = function(ctx)
-    --         if client.name == "svelte" then
-    --           client.notify("$/onDidChangeTsOrJsFile", { uri = ctx.file })
-    --         end
-    --       end,
-    --     })
-    --   end,
-    -- })
+        vim.keymap.set("n", "<leader>lwa", vim.lsp.buf.add_workspace_folder, vim.tbl_extend("force", opts, { desc = "Add workspace folder" }))
+        vim.keymap.set("n", "<leader>lwr", vim.lsp.buf.remove_workspace_folder, vim.tbl_extend("force", opts, { desc = "Remove workspace folder" }))
+        vim.keymap.set("n", "<leader>lwl", function()
+          print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+        end, vim.tbl_extend("force", opts, { desc = "List workspace folders" }))
 
-    -- configure prisma orm server
-    -- lspconfig["prismals"].setup({
-    --   capabilities = capabilities,
-    --   on_attach = on_attach,
-    -- })
-
-    -- configure graphql language server
-    -- lspconfig["graphql"].setup({
-    --   capabilities = capabilities,
-    --   on_attach = on_attach,
-    --   filetypes = { "graphql", "gql", "svelte", "typescriptreact", "javascriptreact" },
-    -- })
-
-    -- configure emmet language server
-    -- lspconfig["emmet_ls"].setup({
-    --   capabilities = capabilities,
-    --   on_attach = on_attach,
-    --   filetypes = { "html", "typescriptreact", "javascriptreact", "css", "sass", "scss", "less", "svelte" },
-    -- })
-
-    -- configure python server
-    lspconfig["pyright"].setup({
-      capabilities = capabilities,
-      on_attach = on_attach,
+        vim.keymap.set({ "n", "v" }, "<leader>la", vim.lsp.buf.code_action, vim.tbl_extend("force", opts, { desc = "See available code actions" }))
+        vim.keymap.set("n", "<leader>lr", vim.lsp.buf.rename, vim.tbl_extend("force", opts, { desc = "Smart rename" }))
+        vim.keymap.set("n", "<leader>lD", "<cmd>Telescope diagnostics bufnr=0<CR>", vim.tbl_extend("force", opts, { desc = "Show buffer diagnostics" }))
+        vim.keymap.set("n", "<leader>ld", vim.diagnostic.open_float, vim.tbl_extend("force", opts, { desc = "Show line diagnostics" }))
+        vim.keymap.set("n", "<leader>lp", vim.diagnostic.goto_prev, vim.tbl_extend("force", opts, { desc = "Go to previous diagnostic" }))
+        vim.keymap.set("n", "<leader>ln", vim.diagnostic.goto_next, vim.tbl_extend("force", opts, { desc = "Go to next diagnostic" }))
+        vim.keymap.set("n", "K", vim.lsp.buf.hover, vim.tbl_extend("force", opts, { desc = "Show documentation for what is under cursor" }))
+        vim.keymap.set("n", "<leader>lx", ":LspRestart<CR>", vim.tbl_extend("force", opts, { desc = "Restart LSP" }))
+        vim.keymap.set("n", "<leader>ls", "<cmd>Telescope spell_suggest<cr>", vim.tbl_extend("force", opts, { desc = "Spell Suggest" }))
+      end,
     })
 
-    lspconfig["bashls"].setup({
+    -- Configure LSP servers using vim.lsp.config (Neovim 0.11+)
+    vim.lsp.config("pyright", {
       capabilities = capabilities,
-      on_attach = on_attach,
     })
 
-    lspconfig["dockerls"].setup({
+    vim.lsp.config("bashls", {
       capabilities = capabilities,
-      on_attach = on_attach,
     })
 
-    lspconfig["marksman"].setup({
+    vim.lsp.config("dockerls", {
       capabilities = capabilities,
-      on_attach = on_attach,
     })
 
-    lspconfig["vimls"].setup({
+    vim.lsp.config("marksman", {
       capabilities = capabilities,
-      on_attach = on_attach,
     })
 
-    -- configure lua server (with special settings)
-    lspconfig["lua_ls"].setup({
+    vim.lsp.config("vimls", {
       capabilities = capabilities,
-      on_attach = on_attach,
-      settings = { -- custom settings for lua
+    })
+
+    vim.lsp.config("lua_ls", {
+      capabilities = capabilities,
+      settings = {
         Lua = {
-          -- make the language server recognize "vim" global
           diagnostics = {
             globals = { "vim" },
           },
           workspace = {
-            -- make language server aware of runtime files
             library = {
               [vim.fn.expand("$VIMRUNTIME/lua")] = true,
               [vim.fn.stdpath("config") .. "/lua"] = true,
@@ -192,5 +84,8 @@ return {
         },
       },
     })
+
+    -- Enable the configured servers
+    vim.lsp.enable({ "pyright", "bashls", "dockerls", "marksman", "vimls", "lua_ls" })
   end,
 }
