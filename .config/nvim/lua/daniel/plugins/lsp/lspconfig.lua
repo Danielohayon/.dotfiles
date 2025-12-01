2de3c9a504a5a8fd9e74312dffde21b429f6a700
Daniel Ohayon <danielohayon@Daniels-MacBook-Pro.local>
2025-12-01 21:05:17 +0200

    bugfix

--- .config/nvim/lua/daniel/plugins/lsp/lspconfig.lua
+++ .config/nvim/lua/daniel/plugins/lsp/lspconfig.lua
@@ -11,11 +11,16 @@ return {
     local capabilities = cmp_nvim_lsp.default_capabilities()
 
     -- Change the Diagnostic symbols in the sign column (gutter)
-    local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
-    for type, icon in pairs(signs) do
-      local hl = "DiagnosticSign" .. type
-      vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
-    end
+    vim.diagnostic.config({
+      signs = {
+        text = {
+          [vim.diagnostic.severity.ERROR] = " ",
+          [vim.diagnostic.severity.WARN] = " ",
+          [vim.diagnostic.severity.HINT] = "󰠠 ",
+          [vim.diagnostic.severity.INFO] = " ",
+        },
+      },
+    })
 
     -- LSP keymaps (set up on LspAttach)
     vim.api.nvim_create_autocmd("LspAttach", {
